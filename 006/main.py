6d7349d022c38b2baae3e1ee3089bc4a5758bc86
Javohir Khamidullaev <97449821+Javohir-dev@users.noreply.github.com>
2024-03-03 20:23:20 +0500

    Lesson: 006

---
+++ 006/main.py
@@ -0,0 +1,84 @@
+from config import API_TOKEN
+import string
+import random
+
+from aiogram import Dispatcher, Bot, executor, types
+
+bot = Bot(API_TOKEN)
+dp = Dispatcher(bot)
+
+
+INFO = "Bu bot sizga hariflarni tasodify tarzda tanlab beradi.\n\nHarfni olish uchun /letter ga bosing"
+START_COMMAND = "<b>👋 Assalomu alaykum,\n</b>Botimisga hush kelibsiz."
+HELP_COMMAND = """
+<b>🆘 Yordam | Bu yerda barcha buyruqlarni ko'rishingiz mumkin.</b>
+
+<b>🔹 Asosiy buyruqlar</b>
+<b>/help</b> - <em>barcha buyruqlar.</em>
+<b>/start</b> - <em>Jarayonni boshlash.</em>
+<b>/info</b> - <em>Bot haqida ma'lumot.</em>
+
+<b>🔸 Yordamchi buyruqlar</b>
+<b>/count</b> - <em>Shunchaki sanash uchun.</em>
+<b>/give</b> - <em>give olish uchun.</em>
+<b>/emoji</b> - <em>Emoji olish uchun.</em>
+
+
+<b>🔥 @ch_wikibot 🔥</b>
+"""
+count = 0
+
+
+async def on_startup(_):
+    print("Bot started successfuly.\nYou can go to the bot and use it.\nOK")
+
+
+# @dp.message_handler(commands=["help"])
+# async def help_command(message: types.Message):
+#     await message.reply(HELP_COMMAND, parse_mode="HTML")
+
+
+@dp.message_handler(commands=["help"])
+async def help_command(message: types.Message):
+    await bot.send_message(
+        chat_id=message.from_user.id, text=HELP_COMMAND, parse_mode="HTML"
+    )
+
+
+@dp.message_handler(commands=["start", "restart"])
+async def welcome(message: types.Message):
+    await message.answer(START_COMMAND, parse_mode="HTML")
+    await message.delete()
+
+
+@dp.message_handler(commands=["image"])
+async def send_image(message: types.Message):
+    await bot.send_photo(
+        chat_id=message.chat.id,
+        photo="https://eurotuner.de/wp-content/uploads/2020/08/Porsche-992-Turbo-S-Sportfedern-Heck-1024x683.jpg",
+    )
+
+
+@dp.message_handler(commands=["location"])
+async def send_point(message: types.Message):
+    await bot.send_location(
+        chat_id=message.from_user.id,
+        latitude=40.899867,
+        longitude=69.318151,
+    )
+    await bot.send_location(
+        chat_id=message.chat.id,
+        latitude=40.899867,
+        longitude=69.318151,
+    )
+    await message.delete()
+
+
+@dp.message_handler()
+async def send_message(message: types.Message):
+    await bot.send_message(chat_id=message.chat.id, text=message)
+    await message.delete()
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
